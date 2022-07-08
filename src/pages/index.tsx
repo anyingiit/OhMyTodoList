322939e564b44ecde3a8739111a9247981832ecd
anyingiit <anyingiit@gmail.com>
2022-07-09 01:26:50 +0800

    feat($data): conform how to from axios send get request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import {GetServerSideProps} from "next";
+import axios from "axios";
+
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
 
 interface Props {
-  abc: boolean
+  todos: Todo[]
 }
 
 export default class Index extends React.Component<Props, any> {
@@ -10,16 +18,34 @@ export default class Index extends React.Component<Props, any> {
     return (
       <div>
         <div>hello world!</div>
-        <div>{this.props.abc ? 'true' : 'false'}</div>
+        {
+          this.props.todos.map((item, index) => {
+            return (
+              <div className={`mb-5 border-2 border-amber-500`} key={index}>
+                <div>{item.id}</div>
+                <div>{item.title}</div>
+              </div>
+            )
+          })
+        }
       </div>
     );
   }
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context): Promise<{ props: Props }> => {
+  let todos: Todo[] = []
+  await axios.get('https://jsonplaceholder.typicode.com/todos/')
+    .then((response) => {
+      todos = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
   return {
     props: {
-      abc: true
-    } as Props
+      todos
+    }
   }
 }
