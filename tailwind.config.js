985baa01820eec6fa1b2f6c7036c64a1f0279935
anyingiit <anyingiit@gmail.com>
2022-07-08 20:56:12 +0800

    feat($Tailwindcss): add Tailwindcss support

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,tsx,tsx}",
+    "./src/component/**/*.{js,ts,tsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
