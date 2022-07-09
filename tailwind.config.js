36293db30f24d624448e2d2c0ba20f3d6b86db23
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-07-09 11:30:37 +0800

    fix($tailwind): tailwind not work with components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,tsx,tsx}",
-    "./src/component/**/*.{js,ts,tsx,tsx}"
+    "./src/components/**/*.{js,ts,tsx,tsx}"
   ],
   theme: {
     extend: {},
