c938ce1e9f2a78f1eecda5bcd4c149025972f353
anyingiit <anyingiit@gmail.com>
2022-07-10 22:05:32 +0800

    feat($menu): remove menu item debug background, change Menu component background beautiful

--- src/components/nav/menu.tsx
+++ src/components/nav/menu.tsx
@@ -16,17 +16,18 @@ interface Props {
 export default class Menu extends React.Component<Props, any> {
   render() {
     return (
-      <div className={`absolute w-10/12 h-full bg-white px-3`}>
+      <div className={`absolute w-10/12 h-full bg-white px-3 bg-neutral-200`}>
         <div className={`h-16 w-full flex items-center`}>
           <MenuButton onClickFunc={this.props.closeMenuFunc}/>
         </div>
         {
           this.props.tasks.map((item, index) => {
             return (
-              <div className={`h-16 w-full bg-red-500 flex flex-row items-center`} onClick={() => {
-                item.func()
-                this.props.closeMenuFunc()
-              }} key={index}>
+              <div className={`h-16 w-full flex flex-row items-center box-border border-b border-gray-500`}
+                   onClick={() => {
+                     item.func()
+                     this.props.closeMenuFunc()
+                   }} key={index}>
                 <div className={`relative h-8 w-8`}>
                   <Image src={item.iconSrc} layout={`fill`}/>
                 </div>
