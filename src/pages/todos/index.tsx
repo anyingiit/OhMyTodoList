cbeb85e3f0dfe3576d3aff06f6252fa9e5550049
anyingiit <anyingiit@gmail.com>
2022-07-14 01:10:45 +0800

    feat($todos): use todoBase build this page, this page can automatically have`全部任务, 未完成, 已完成`method

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Main from "../../components/main";
 import {GetServerSideProps} from "next";
 import {Todo, User} from "../../types/typs";
+import TodoBase from "../../components/todoBase";
 
 interface Props {
   data: {
@@ -15,75 +15,20 @@ interface Props {
 
 // todos/Index: 用于显示全部任务
 export default class Index extends React.Component<Props, any> {
-  //TODO: 全部任务, 未完成, 已完成 这种统一的任务应当再封装一层
-  updateTaskSelectedState = (newSelectedTaskIndex: number) => {
-    let tasks = this.state.tasks
-    tasks = tasks.map((item, index) => {
-      item.selected = index === newSelectedTaskIndex;
-      return item
-    })
-    this.setState({tasks: tasks, navData: {title: tasks[newSelectedTaskIndex].name}})
-  }
-
-  changeTodoListDataToAllTodo = () => {
-    const data = this.props.data
-    this.updateTaskSelectedState(0)
-    this.setState({data: data})
-  }
-  changeTodoListDataToUnCompletedTodo = () => {
-    const data = this.props.data.filter((item) => {
-      if (!item.completed) {
-        return true
-      }
-    })
-    this.updateTaskSelectedState(1)
-    // console.log(data)
-    this.setState({data: data})
-  }
-  changeTodoListDataToCompletedTodo = () => {
-    const data = this.props.data.filter((item) => {
-      if (item.completed) {
-        return true
-      }
-    })
-    this.updateTaskSelectedState(2)
-    // console.log(data)
-    this.setState({data: data})
-  }
-
-  state = {
-    data: this.props.data,
-    tasks: [
-      {
-        name: '全部任务',
-        iconSrc: require('../../../public/cloud.png'),
-        func: this.changeTodoListDataToAllTodo,
-        selected: true
-      },
-      {
-        name: '未完成',
-        iconSrc: require('../../../public/cloud.png'),
-        func: this.changeTodoListDataToUnCompletedTodo,
-        selected: false
-      },
-      {
-        name: '已完成',
-        iconSrc: require('../../../public/cloud.png'),
-        func: this.changeTodoListDataToCompletedTodo,
-        selected: false
-      },
-    ],
-    navData: {
-      title: '全部任务',
-      shortCuts: []
-    }
-  }
-
-
   render() {
     return (
-      <Main todoListData={this.state.data} showTodoAvatar={true} todoListTask={this.state.tasks}
-            navData={this.state.navData}/>
+      <TodoBase
+        data={this.props.data}
+        tasks={[]}
+        showTodoAvatar={true}
+        headerData={{
+          data: {
+            title: `Todo List Overview`,
+            href: '/todos'
+          },
+          userAvatarData: undefined
+        }}
+      />
     )
   }
 }
